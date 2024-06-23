71138566a04eceb75101fa69b46a8561e517d865
faroukabichou <faroukabichoudev@gmail.com>
2024-06-23 21:45:16 +0100

    Some changes

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -2,16 +2,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.text.TextStyle
 import kustomize.composeapp.generated.resources.Res
 import kustomize.composeapp.generated.resources.comet
+import kustomize.composeapp.generated.resources.compose_multiplatform
 import org.jetbrains.compose.resources.imageResource
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 
@@ -23,16 +22,11 @@ fun App(
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.Black),
+            .background(color = Color(0xff292A30)),
         contentAlignment = androidx.compose.ui.Alignment.Center
     ){
-        imageResource(
-            Res.drawable.comet,
-        )
-        Image(
-            ImageBitmap = imageResource(Res.drawable.comet),
-            contentDescription = "comet"
-        )
+        Image(painterResource(Res.drawable.comet), null)
+
     }
 //    MaterialTheme(
 ////        typography = typography,
